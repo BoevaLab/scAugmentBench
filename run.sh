bd90775715442213a5d83459a836d042e2a85df4
travelingtomat0 <62526202+travelingtomat0@users.noreply.github.com>
2024-10-28 19:33:13 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
+# Tuning Representation Dimension and Projection Dimension
+echo "This is an example to find parameters for the simclr method"
+python main.py --multirun +exp=simclr_ablation
+
+
+# Ablation of Augmentation Hyperparameters
+echo "This is an example to tune augmentation-parameters CLEAR-augmentations"
 python main.py --multirun +aug_exp=combi-bbknn-all
 python main.py --multirun +aug_exp=combi-gauss-all
 python main.py --multirun +aug_exp=combi-mask-all
 python main.py --multirun +aug_exp=combi-swap-all
+
+# Benchmarking Unimodal Batch-Correction
+python main.py --multirun +exp=all_datasets
+
+# Unimodal Query-Reference Mapping Benchmark
+python main.py --multirun +exp=qr_big
+python main.py --multirun +exp=qr_small
